8475a79be01f0dd5e79ed399d51a8aa818bdcb56
Larissa de Oliveira <69212760+lariadeoliveira@users.noreply.github.com>
2025-01-24 10:30:02 -0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,11 +1,7 @@
-from django.urls import path
-from .views import RegisterView, LoginView, UserBalanceView, AddBalanceView, TransferView, TransferListView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('balance/', UserBalanceView.as_view(), name='user_balance'),
-    path('add-balance/', AddBalanceView.as_view(), name='add_balance'),
-    path('transfer/', TransferView.as_view(), name='transfer'),
-    path('transfers/', TransferListView.as_view(), name='transfer_list'),
+    path('admin/', admin.site.urls),
+    path('api/autenticacao/', include('meu_projeto.apps.autenticacao.urls')),
 ]
